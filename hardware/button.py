e566df5c63487ac1e3158a067ad812add2c355e7
kthorne44 <72312351+kthorne44@users.noreply.github.com>
2023-02-04 15:58:59 -0500

    added second button

--- hardware/button.py
+++ hardware/button.py
@@ -1,13 +1,23 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import time
 
-num = 0
-last_press = time.time()
+taken_num = 0
+snooze_num = 0
+t_last_press = time.time()
+snooze_press = time.time()
 
-def button_callback(channel):
+def taken_button_callback(channel):
     global last_press
-    if(time.time() - last_press > 3):
-        last_press = time.time()
+    if(time.time() - t_last_press > 3):
+        t_last_press = time.time()
+        global num
+        num = num + 1
+        print(num)
+
+def snooze_button_callback(channel):
+    global snooze_press
+    if(time.time() - snooze_press > 3):
+        snooze_press = time.time()
         global num
         num = num + 1
         print(num)
@@ -15,7 +25,9 @@ def button_callback(channel):
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(10,GPIO.RISING,callback=taken_button_callback) # Setup event on pin 10 rising edge
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 8 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(8,GPIO.RISING,callback=snooze_button_callback) # Setup event on pin 8 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
