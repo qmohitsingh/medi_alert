77a277eaa0d543826a26d3d7eec755cb845d3198
kthorne44 <72312351+kthorne44@users.noreply.github.com>
2023-02-04 14:20:53 -0500

    updating servo code to rotate 180 once

--- hardware/moter.py
+++ hardware/moter.py
@@ -12,7 +12,7 @@ servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
 #start PWM running, but with value of 0 (pulse off)
 servo1.start(0)
 print ("Waiting for 2 seconds")
-time.sleep(2)
+time.sleep(1)
 
 #Let's move the servo!
 print ("Rotating 180 degrees in 10 steps")
@@ -23,16 +23,16 @@ duty = 2
 # Loop for duty values from 2 to 12 (0 to 180 degrees)
 while duty <= 12:
     servo1.ChangeDutyCycle(duty)
-    time.sleep(1)
+    time.sleep(0.010)
     duty = duty + 1
 
 # Wait a couple of seconds
-time.sleep(2)
+time.sleep(1)
 
 # Turn back to 90 degrees
 print ("Turning back to 90 degrees for 2 seconds")
 servo1.ChangeDutyCycle(7)
-time.sleep(2)
+time.sleep(0.05)
 
 #turn back to 0 degrees
 print ("Turning back to 0 degrees")
